fece65e5f6c0ab26071ee05ed35f2251728d55e7
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-12-23 16:12:44 +0900

    [Update]タイムライン完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 
   def show
@@ -7,4 +8,12 @@ class TweetsController < ApplicationController
 
   def new
   end
+
+  def create
+    @tweet = Tweet.new
+        @tweet.content = params[:tweet][:content]
+        @tweet.save
+        redirect_to tweets_index_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # User
+  devise_for :users
   get 'users/index'
   get 'users/show'
   get 'users/show/:username' => 'users#show'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Tweet
+  get 'tweets/index'
+  get 'tweets/show'
+  get 'tweets/new'
+  post 'tweets' => 'tweets#create'
+  # Teamroom
   get 'rooms/show'
 end
