8adf6e4084d24888047528c137855ef79536d5e0
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-12-23 11:09:41 +0900

    [Update]Userカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_035704) do
+ActiveRecord::Schema.define(version: 2019_12_23_015810) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2019_12_22_035704) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.string "location"
+    t.text "about"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
